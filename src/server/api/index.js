4273b690684feb89eb6e88cbd94235615dcb6d9d
zrobit <luismigre47@gmail.com>
2017-01-27 23:23:49 -0500

    pluralize url for consitence naming

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -2,7 +2,7 @@ var express = require('express')
   , router = express.Router()
 
 router.use('/quotes', require('./quoteApi'))
-router.use('/author', require('./authorApi'))
+router.use('/authors', require('./authorApi'))
 // router.use('/cars', require('./cars'))
 
 module.exports = router

64c420bf9ec8fc0c30f587fccb02bb9b4abe0f3c
zrobit <luismigre47@gmail.com>
2017-01-27 23:24:31 -0500

    add authorController

---
+++ src/server/controllers/authorController.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const axios = require('axios');
+const api = axios.create({
+  baseURL: 'http://localhost:3000/api/'
+});
+
+const ssr = global.ssr
+
+const router = express.Router();
+
+
+router.get('/:slug', function(req, res){
+  let slug = req.params.slug
+
+  let context = {};
+
+  api.get('/authors/' + slug)
+    .then(function(response){
+      let state = {
+        quotes: response.data
+      }
+      context.state = state;
+      ssr(req, res, context)
+    })
+});
+
+
+module.exports = router;
--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -3,6 +3,7 @@ var express = require('express')
 
 router.use('/', require('./homeController'))
 router.use('/frase', require('./quoteController'))
+router.use('/autor', require('./authorController'))
 // router.use('/about', require('./about'))
 
 module.exports = router

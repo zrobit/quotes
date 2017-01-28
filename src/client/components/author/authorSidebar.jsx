a99af9b4ddd3c760638b3e9831deec0544112b63
zrobit <luismigre47@gmail.com>
2017-01-27 22:52:29 -0500

    add authorSidebar.jsx componenet

---
+++ src/client/components/author/authorSidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'react-router'
+import AuthorDisplay from '../author/authorDisplay'
+
+export default () => (
+  <div className="sidebar">
+    <AuthorDisplay />
+  </div>
+);

ea2ef28e98c47795c8a5f8b8f7ed161a99c8e0cf
zrobit <luismigre47@gmail.com>
2017-01-31 14:26:27 -0500

    add virtual to schema

--- src/server/models/author.js
+++ src/server/models/author.js
@@ -3,21 +3,33 @@ const Schema = mongoose.Schema;
 
 const slug = require('slugg')
 
+const config = {
+  timestamps: true,
+  toJSON : {virtuals:true},
+  id: false
+}
+
+
 var authorSchema = new Schema({
   name: String,
   slug: String,
-}, {timestamps: true});
+  bio: {
+    avatar: String,
+    resume: String,
+    meta:[{_id:false, label:String, value:String}]
+  }
+}, config);
 
 authorSchema.pre('save', function(next) {
   this.slug = slug(this.name);
   next();
 });
 
-// authorSchema.virtual('quotes', {
-//   ref: 'Quote',
-//   localField: '_id',
-//   foreignField: 'author'
-// });
+authorSchema.virtual('quotes', {
+  ref: 'Quote',
+  localField: '_id',
+  foreignField: 'author'
+});
 
 var Author = mongoose.model('Author', authorSchema);
 module.exports = Author;

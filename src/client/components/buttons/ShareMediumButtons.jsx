0f50865389815403a08e4b8afbb605c33e377d18
zrobit <luismigre47@gmail.com>
2017-02-01 16:40:19 -0500

    update styles buttons

--- src/client/components/buttons/ShareMediumButtons.jsx
+++ src/client/components/buttons/ShareMediumButtons.jsx
@@ -4,11 +4,11 @@ import TwitterMediumButton from './TwitterMediumButton';
 import style from './buttons.styl'
 
 export default () => (
-  <ul className={style.mediumBtnList}>
-    <li>
+  <ul className={style.shareMedium}>
+    <li className={style.left} >
       <FacebookMediumButton url='/test/facebook' />
     </li>
-    <li>
+    <li className={style.right}>
       <TwitterMediumButton url='/test/twitter'/>
     </li>
   </ul>

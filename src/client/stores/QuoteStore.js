c5be0555cfc1515e8de69e872f2ad26364c143a7
zrobit <luismigre47@gmail.com>
2017-02-01 16:41:00 -0500

    add fetchData from server api

--- src/client/stores/QuoteStore.js
+++ src/client/stores/QuoteStore.js
@@ -1,24 +1,40 @@
 import {observable, computed, action} from "mobx";
 import QuoteModel from '../models/QuoteModel'
+import axios from 'axios'
 
 export default class QuoteStore {
   @observable quotes = [];
   quote = {};
   author = {};
   section = '';
+  @observable isLoading = false;
 
   setQuoteDetail(quote){
     this.quote = quote;
   }
   setAuthorDetail(author){
     this.author = author
+    this.fetchAuthor();
+  }
+
+  fetchAuthor(){
+    let self = this;
+    self.isLoading = true;
+    axios.get('/api/authors/' + self.author.slug)
+      .then(function (response) {
+        self.author = response.data.author
+        self.isLoading = false;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   static fromJS(data) {
     const quoteStore = new QuoteStore();
     if (data.type === 'authorModel') {
       quoteStore.author = data.author
-      quoteStore.quotes = data.quotes.map(item => QuoteModel.fromJS(quoteStore, item))
+      quoteStore.quotes = data.author.quotes.map(item => QuoteModel.fromJS(quoteStore, item))
 
     } else if (data.constructor === Array){
       quoteStore.quotes = data.map(item => QuoteModel.fromJS(quoteStore, item))

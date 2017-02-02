aba72ecb9366979f23bcf3901ce382b1360044ba
zrobit <luismigre47@gmail.com>
2017-02-02 14:09:23 -0500

    Feature: task seed:quotes now add fake tags for developing process

--- tasks/seed.js
+++ tasks/seed.js
@@ -4,14 +4,16 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/dot-quotes');
 
-
 const fake = require('casual');
+
 const Author = require('../src/server/models/author');
 const Quote = require('../src/server/models/quote');
+const Tag = require('../src/server/models/tag');
 
 const cicle = require('../src/utils').cicle
 const fakeAuthors = require('../src/utils/data').fakeAuthors
 const fakeQuotes = require('../src/utils/data').fakeQuotes
+const fakeTags = require('../src/utils/data').fakeTags
 
 
 const db = mongoose.connection;
@@ -26,19 +28,28 @@ gulp.task('connectDB', function(cb) {
 
 gulp.task('seed:quotes', ['connectDB'], function(){
   let authors = fakeAuthors(10);
-  let it = cicle(authors)
-  let quotes = fakeQuotes(20)
+  let it = cicle(authors);
+  let quotes = fakeQuotes(20);
+  let tags = fakeTags(10);
 
-  quotes.forEach((element) => {
-    Author.create(it.next(), (err, author)=> {
-      if (err) throw err;
-      console.log("Author: " + author)
-      element.author = author.id
-      Quote.create(element, (err, quote) => {
+  Tag.create(tags, function(err, tags){
+    if(err) throw err;
+    let itTags = cicle(tags);
+    quotes.forEach((element) => {
+      Author.create(it.next(), (err, author)=> {
         if (err) throw err;
-        console.log("Quote: " + quote)
-      })
-    })
+        element.author = author.id
+        Quote.create(element, (err, quote) => {
+          if (err) throw err;
+          for(let i = 0; i < 3; i++){
+            quote.tags.push(itTags.next());
+          }
+          quote.save(function(err, finalData){
+            console.log(finalData)
+          });
+        });
+      });
+    });
   });
 });
 
@@ -54,7 +65,6 @@ gulp.task('seed:author', ['connectDB'], function(){
 });
 
 //Todo implementar interactive para estar seguros si queremos eleminar
-
 //Eliminar Collections
 gulp.task('seed:author:clear', ['connectDB'], function(){
 

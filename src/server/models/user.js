412cc307d7550e812b1dc5d351b5a20ac9bc0e12
zrobit <luismigre47@gmail.com>
2017-02-02 20:31:38 -0500

    add initial user model

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,5 +1,19 @@
-module.exports = {
-  attributes: {
-    name: 'string'
-  }
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const config = {
+  timestamps: true
 }
+
+const userSchema = new Schema({
+  username: String,
+  email: {type: String, required: true),
+  password: String,
+  firstName: {type: String, },
+  lastName: {type: String, },
+  password: {type: String, required: true },
+}, config);
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;

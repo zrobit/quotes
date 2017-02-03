7f14b96c364c2b507aa81fe74cdffc412676fbe3
zrobit <luismigre47@gmail.com>
2017-02-02 19:11:00 -0500

    Refactor: rename componet following Airbnb style guide

--- src/client/routes.jsx
+++ src/client/routes.jsx
@@ -4,13 +4,13 @@ import { Route } from 'react-router';
 import Main from './components/main';
 import AuthorSection from './components/author/AuthorSection';
 import QuoteSection from './components/quote/QuoteSection';
-import Login from './components/auth/login';
+import LoginSection from './components/auth/LoginSection';
 
 export default (
   <Route path="/" component={Main}>
     <Route path="/autor/:slug" component={AuthorSection} />
     <Route path="/frase/:slug" component={QuoteSection} />
-    <Route path="/login" component={Login} />
+    <Route path="/login" component={LoginSection} />
   </Route>
 );
 

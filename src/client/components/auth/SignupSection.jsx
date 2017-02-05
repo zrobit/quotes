393ceae9fb3f3ab761c5fc77854a557cab7b3f0b
zrobit <luismigre47@gmail.com>
2017-02-05 17:25:26 -0500

    Feature: add SignupSection

---
+++ src/client/components/auth/SignupSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import FormSignup from './FormSignup'
+import WrapAuth from './WrapAuth'
+
+function SignupSection() {
+  return (
+    <WrapAuth>
+      <FormSignup action="/signup"/>
+    </WrapAuth>
+  );
+}
+
+export default SignupSection;

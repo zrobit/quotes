dd4a955e853d2af73efb052da5b35810319ce9e9
zrobit <luismigre47@gmail.com>
2017-02-05 17:16:28 -0500

    Refactor: component now manage the state for all subcomponents

--- src/client/components/auth/FormLogin.jsx
+++ src/client/components/auth/FormLogin.jsx
@@ -1,29 +1,58 @@
 import React, { Component } from 'react'
 import {inject, observer} from "mobx-react";
 
-
-import WrapAuth from './wrapAuth'
-import ButtonAuth from './buttonAuth'
-import EmailField from './emailField';
-import PasswordField from './passwordField';
-
+import EmailField from './EmailField'
+import PasswordField from './PasswordField'
+import ButtonAuth from './ButtonAuth'
 
 @inject('loginStore') @observer
-class FormAuth extends Component {
-  render() {
-    const {loginStore} = this.props
+class FormSignup extends Component {
+  constructor(props){
+    super(props)
+    this.loginStore = this.props.loginStore
+    this.EmailChange = this.EmailChange.bind(this);
+    this.EmailBlur = this.EmailBlur.bind(this);
+    this.PasswordChange = this.PasswordChange.bind(this);
+    this.PasswordBlur = this.PasswordBlur.bind(this);
+  }
+
+  EmailChange(e){
+    this.loginStore.emailValue = e.target.value
+
+  }
+
+  EmailBlur(e){
+    this.loginStore.emailValidate(e.target.value)
+  }
+
+  PasswordChange(e){
+    this.loginStore.passwordValue = e.target.value
+  }
+
+  PasswordBlur(e){
+    this.loginStore.passwordValidate(e.target.value)
+  }
+
+  render (){
+    const {action} = this.props
     return (
-      <WrapAuth>
-        <form className="form" method="post" action={this.props.action}>
-          <div className="row">
-            <EmailField loginStore={loginStore}/>
-            <PasswordField loginStore={loginStore}/>
-            <ButtonAuth display="SIGNUP"/>
-          </div>
-        </form>
-      </WrapAuth>
+      <form className="form" method="post" action={action}>
+        <EmailField
+          value={this.loginStore.emailValue}
+          onChange={this.EmailChange}
+          onBlur={this.EmailBlur}
+          error={this.loginStore.emailError}
+        />
+        <PasswordField
+          value={this.loginStore.passwordValue}
+          onChange={this.PasswordChange}
+          onBlur={this.PasswordBlur}
+          error={this.loginStore.passwordError}
+        />
+        <ButtonAuth display="Registar" />
+      </form>
     );
   }
 }
 
-export default FormAuth
+export default FormSignup;

5bdc6499b4d9573fe7fae9d3cb23c2cec1ea67d1
zrobit <luismigre47@gmail.com>
2017-02-05 17:03:30 -0500

    Refactor: transform component to stateless

--- src/client/components/auth/emailField.jsx
+++ src/client/components/auth/emailField.jsx
@@ -1,47 +1,24 @@
 import React, { Component } from 'react';
 import {inject, observer} from "mobx-react";
 
-
-import EmailInpunt from './emailInput';
 import ErrorField from './errorField';
 
-
-@observer
-class EmailField extends Component {
-
-  constructor(props){
-    super(props)
-    this.loginStore = this.props.loginStore
-    this.handleChange = this.handleChange.bind(this);
-    this.handleBlur = this.handleBlur.bind(this);
-  }
-
-  handleChange (e) {
-    this.loginStore.emailValue = e.target.value
-  }
-
-  handleBlur (e) {
-    this.loginStore.emailValidate(e.target.value)
-  }
-
-  render() {
-    const {emailValue, emailError} = this.props.loginStore
-    return (
-      <div>
-        <label>Email</label>
-        <EmailInpunt
-          value={emailValue}
-          handleChange={this.handleChange}
-          handleBlur={this.handleBlur}
-        />
-        {emailError ?
-          <ErrorField message='Correo electrónico no valido'/>
-          :
-          null
-        }
-      </div>
-    )
-  }
+function EmailField ({error, ...props}) {
+  return (
+    <div>
+      <label>Email</label>
+      <input
+        className="in-text"
+        type="text"
+        name="email"
+        {...props}
+      />
+      {error
+        ? <ErrorField message={error}/>
+        : null
+      }
+    </div>
+  )
 }
 
 export default EmailField;

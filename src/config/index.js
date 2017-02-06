698d18b7d80020f2b94d0a2f8fa5e40ac8744933
zrobit <luismigre47@gmail.com>
2017-02-06 09:32:46 -0500

    Refactor: moving paths to config module

--- src/config/index.js
+++ src/config/index.js
@@ -7,6 +7,11 @@ const basePath = path.resolve(__dirname)
 const srcPath = path.resolve(basePath, '../')
 const projectPath = path.resolve(srcPath, '../')
 
-const assetsPath = path.join(srcPath, 'assets')
+const assetsPath = path.join(srcPath, 'client/assets')
+const assetsPublic = path.join(projectPath, 'dist/public/assets')
 
 exports.MEDIA_IMAGES = path.join(projectPath, 'media/images')
+exports.ASSETS_IMAGES = path.join(assetsPath, 'images')
+exports.ASSETS_FONTS = path.join(assetsPath, 'fonts')
+exports.ASSETS_STYLES = path.join(assetsPath, 'styles')
+exports.PUBLIC_STYLES = path.join(assetsPublic, 'styles')
--- src/server/middleware/static.js
+++ src/server/middleware/static.js
@@ -7,8 +7,8 @@ var router = express.Router();
 
 router.use('/styles', stylus.middleware({
   debug: true,
-  src: __dirname + '/../../assets/styles',
-  dest: __dirname + '/../../../dist/public/assets/styles',
+  src: config.ASSETS_STYLES,
+  dest: config.PUBLIC_STYLES,
   serve: true,
   forse: false,
   compile: function (str, path) {
@@ -21,11 +21,11 @@ router.use('/styles', stylus.middleware({
 //   res.json(__dirname + '/../../build/public/assets/styles')
 
 // });
-router.use('/styles', express.static(__dirname+'/../../../dist/public/assets/styles'));
+router.use('/styles', express.static(config.PUBLIC_STYLES));
 // router.use('/scripts', express.static(__dirname + '/../../build/public/assets/scripts'));
-router.use('/images', express.static(__dirname + '/../assets/images'));
+router.use('/images', express.static(config.ASSETS_IMAGES));
 router.use('/media/images', express.static(config.MEDIA_IMAGES));
-router.use('/fonts', express.static(__dirname + '/../assets/fonts'));
+router.use('/fonts', express.static(config.ASSETS_FONTS));
 
 
 module.exports = router;

3105bdf500e46fd01e70a0392655cf2c4b56fb14
zrobit <luismigre47@gmail.com>
2017-02-06 14:36:54 -0500

    Refactor: delete dead code

--- src/server/router.js
+++ src/server/router.js
@@ -1,27 +1,16 @@
 var express = require('express');
 var router = express.Router();
-// var isoMiddleware = require('./middleware').isoMiddleware
-// import { isoMiddleware } from '../shared/iso2';
-
-
-
 
 if (process.env.NODE_ENV==='build'){
-  // var isoMiddleware = require('../../dist/shared/iso2').isoMiddleware;
   global.ssr = require('../../dist/shared/ssr').ssr
 
 } else {
-  // var isoMiddleware = require('../shared/iso2').isoMiddleware;
   global.ssr = require('../shared/ssr').ssr
-
 }
-
 
 router.use('/assets', require('./middleware/static'))
 
 router.use('/api', require('./api'))
-
-// router.use(isoMiddleware)
 
 router.use('/', require('./controllers'))
 

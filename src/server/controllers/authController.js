bc3de12952e0b60268022e0496dd7c5859a46fdf
zrobit <luismigre47@gmail.com>
2017-02-07 00:29:43 -0500

    Feature: add authenticate local strategy

--- src/server/controllers/authController.js
+++ src/server/controllers/authController.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const axios = require('axios');
+const passport = require('passport');
 
-const ssr = global.ssr
+
+const ssr = global.ssr;
 
 const router = express.Router();
 
 
 router.get('/login', function(req, res){
   let context = {
-    state:{}
+    state:{
+      auth: {
+        error: req.flash('loginMessage')[0]
+      }
+    }
   };
   ssr(req, res, context)
 });
 
-router.post('/login', function(req, res){
-
-});
+router.post('/login', passport.authenticate('local-login', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
 
 router.get('/signup', function(req, res){
   let context = {

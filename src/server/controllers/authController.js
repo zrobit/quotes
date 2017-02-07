b585a6df72f714dac833e7477b80636f394fa98e
zrobit <luismigre47@gmail.com>
2017-02-07 15:56:45 -0500

    Feature: add signup handler

--- src/server/controllers/authController.js
+++ src/server/controllers/authController.js
@@ -9,6 +9,9 @@ const router = express.Router();
 
 
 router.get('/login', function(req, res){
+  if (req.isAuthenticated()) {
+    return res.redirect('/');
+  }
   let field = req.query.field;
   if(field){
     if(!isEmail(field)){res.redirect('/login')}
@@ -25,10 +28,12 @@ router.get('/login', function(req, res){
 });
 
 router.post('/login', function(req, res, next) {
+  let password = req.body.password;
+  let email= req.body.email;
+
   passport.authenticate('local-login', function(err, user, info) {
     if (err) { return next(err); }
     if (!user) {
-      let email = req.body.email;
       let url = isEmail(email) ? '/login?field='+ email : '/login';
       return res.redirect(url);
     }
@@ -40,22 +45,46 @@ router.post('/login', function(req, res, next) {
 });
 
 router.get('/signup', function(req, res){
+  if (req.isAuthenticated()) {
+    return res.redirect('/');
+  }
+
   let context = {
     state:{
       auth: {
-        ref: 'SignupSection'
+        ref: 'SignupSection',
+        error: req.flash('loginMessage')[0]
       }
     }
   };
   ssr(req, res, context)
 });
 
-router.post('/signup', function(req, res){
+router.post('/signup', function(req, res, next){
+  let password = req.body.password;
+  let email= req.body.email;
 
+  if(!email || !password) {
+    return res.status(403).send('Forbiden(403): invalid values :V')
+  } else if(!isEmail(email) || (password.length < 8)){
+    return res.status(403).send('Forbiden(403): invalid values :V')
+  }
+
+  passport.authenticate('local-signup', function(err, user, info) {
+    if (err) { return next(err); }
+    if (!user) {
+      return res.redirect('/signup');
+    }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      return res.redirect('/');
+    });
+  })(req, res, next);
 });
 
 router.get('/logout', function(req, res){
-
+  req.logout();
+  res.redirect('/');
 });
 
 

2177f91dc7b161eb822f36f0f7279ba604f2270b
zrobit <luismigre47@gmail.com>
2017-02-07 00:26:41 -0500

    Feature: add bcrypt support for hashing passwords

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,5 +1,7 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bcrypt = require('bcrypt');
 
 const config = {
   timestamps: true
@@ -13,6 +15,19 @@ const userSchema = new Schema({
   lastName: {type: String, },
   password: {type: String, required: true },
 }, config);
+
+userSchema.pre('save', function (next) {
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(this.password, salt, function(err, hash) {
+      if (err) {
+        throw err;
+      } else {
+        this.password = hash;
+        next();
+      }
+    });
+  });
+});
 
 var User = mongoose.model('User', userSchema);
 

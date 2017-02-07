46179289389de3445ec9dec3981333787930be58
zrobit <luismigre47@gmail.com>
2017-02-07 00:23:35 -0500

    Feature: add session flash and passport support

--- src/server/app.js
+++ src/server/app.js
@@ -1,23 +1,40 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash    = require('connect-flash');
 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session      = require('express-session');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/dot-quotes');
 
 
 var router = require('./router')
 
+require('./config/passport');
+
 
 app.locals = {pretty:true, cache:true}
 
-
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
 app.use(bodyParser.json()); // support json encoded bodies
+
+// required for passport
+app.use(session({
+  secret: 'thesewordsareuniquedontcopyXD',
+  resave: false,
+  saveUninitialized: false
+})); // session secret
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.use(router)
 

88f6398cfeee525467b67911c1f1771c8dd73de6
zrobit <luismigre47@gmail.com>
2017-02-09 10:41:17 -0500

    Feature: add inital searchApi for managing search queries

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -6,5 +6,6 @@ router.use('/quotes', require('./quoteApi'))
 router.use('/authors', require('./authorApi'))
 router.use('/tags', require('./tagApi'))
 router.use('/users', require('./userApi'))
+router.use('/search', require('./searchApi'))
 
 module.exports = router
---
+++ src/server/api/searchApi.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+const Quote = require('../models/quote');
+
+router.get('/', function(req, res, next){
+  const query = req.query.q;
+  Quote
+  .find({
+    $text: {$search: query}
+  })
+  .limit(10).sort({'createdAt':-1})
+  .select('slug content author tags')
+  .populate('tags', 'name slug -_id')
+  .populate('author', 'name slug')
+  .exec((err, data) => {
+    if (err) throw err;
+    let context = {
+      quotes: data
+    };
+    res.json(context)
+  })
+});
+
+module.exports = router;

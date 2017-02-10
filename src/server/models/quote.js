107133e9099f737f768a423bf66fa1d05d29f1ec
zrobit <luismigre47@gmail.com>
2017-02-10 16:08:49 -0500

    Feature: add sizes of text for styling with

--- src/server/models/quote.js
+++ src/server/models/quote.js
@@ -7,11 +7,19 @@ const config = {
 
 var quoteSchema = new Schema({
   content: {type: String, text: true},
+  size: {type: String, enum:['tiny', 'small', 'medium', 'large']},
   slug: String,
   author: { type: Schema.Types.ObjectId, ref: 'Author' },
   tags: [{type: Schema.Types.ObjectId, ref: 'Tag'}]
 }, config);
 
+quoteSchema.pre('save', function (next) {
+  let self = this;
+  let len = self.content.length
+  self.size = [
+  'large', 'medium', 'small', 'tiny'
+  ][[180, 120, 60, 0].findIndex(elem => len > elem)];
+});
 
 var Quote = mongoose.model('Quote', quoteSchema);
 module.exports = Quote;

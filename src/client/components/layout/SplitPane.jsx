c4670a229d739d38d19912b9692eaf688e7b8c7d
zrobit <luismigre47@gmail.com>
2017-02-11 17:51:32 -0500

    Feature: resposive order left bar to firt elemente

--- src/client/components/layout/SplitPane.jsx
+++ src/client/components/layout/SplitPane.jsx
@@ -7,11 +7,11 @@ import cx from 'classnames'
 export default ({main, sidebar}) => (
   <div className={style.wrap}>
     <div className={cx(style.inner, 'clearfix')}>
-      <div className={style.main}>
-        {main}
-      </div>
       <div className={style.sidebar}>
         {sidebar}
+      </div>
+      <div className={style.main}>
+        {main}
       </div>
     </div>
   </div>

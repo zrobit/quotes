16e3e2a8c25142bc56d8265a323c00bb2706fbd8
zrobit <luismigre47@gmail.com>
2017-05-07 02:10:13 -0500

    Refactor: change from Mobx to Redux

--- src/client/client.jsx
+++ src/client/client.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'mobx-react';
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+import reducer from './reducers';
+
+
 import { Router, browserHistory} from 'react-router';
 import routes from './routes.jsx';
 
-import AppStore from './stores/AppStore';
-import AuthStore from './stores/AuthStore';
 
-
-// const initialState = window.initialState && JSON.parse(window.initialState) || {};
 const initialState = window.initialState || {};
 
-// var viewStore = new ViewStore();
-
-const stores = {
-  appStore: AppStore.fromJS(initialState.app || []),
-  authStore: new AuthStore(initialState.auth)
-}
+let store = createStore(reducer, initialState, applyMiddleware(thunk, logger))
+// let store = createStore(reducer, initialState, applyMiddleware(thunk))
 
 render(
-  <Provider {...stores}>
+  <Provider store={store}>
     <Router routes={routes} history={browserHistory} />
   </Provider>,
   document.getElementById('root')

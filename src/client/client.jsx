c6098f365d7b431d5f908f356c168e3937979a21
zrobit <luismigre47@gmail.com>
2017-05-10 16:21:17 -0500

    Refactor: roll back to mobx store

--- src/client/client.jsx
+++ src/client/client.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
+import { Provider } from 'mobx-react';
 
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-
-import reducer from './reducers';
-
+import createStores from './stores'
 
 import { Router, browserHistory} from 'react-router';
 import routes from './routes.jsx';
@@ -16,11 +11,10 @@ import routes from './routes.jsx';
 
 const initialState = window.initialState || {};
 
-let store = createStore(reducer, initialState, applyMiddleware(thunk, logger))
-// let store = createStore(reducer, initialState, applyMiddleware(thunk))
+const stores = createStores(initialState);
 
 render(
-  <Provider store={store}>
+  <Provider {...stores}>
     <Router routes={routes} history={browserHistory} />
   </Provider>,
   document.getElementById('root')

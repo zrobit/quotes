824c45c627cbd583178af92ca1c0db1569abba1e
zrobit <luismigre47@gmail.com>
2017-05-19 15:05:56 -0500

    Add: comment code for switching routes easily

--- src/client/routes.jsx
+++ src/client/routes.jsx
@@ -8,17 +8,15 @@ import LoginSection from './components/auth/LoginSection';
 import SignupSection from './components/auth/SignupSection';
 // import ProfileSection from './components/profile/ProfileSection';
 
-// import Refactor from './__refactor__';
-
 
 export default (
   <Route path="/" component={Main}>
     <Route path="/signup" component={SignupSection} />
     <Route path="/login" component={LoginSection} />
-    {/*
     <Route path="/frase/:slug" component={QuoteSection} />
     <Route path="/autor/:slug" component={AuthorSection} />
     <Route path="/search" component={Main} />
+    {/*
     <Route path="/perfil/:user" component={ProfileSection} />
     */}
   </Route>

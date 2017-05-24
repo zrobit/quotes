da9d7665b2a723cf7286ecf5f204a16d15cdeafd
zrobit <luismigre47@gmail.com>
2017-05-24 17:53:58 -0500

    Add: new route for pagination

--- src/client/routes.jsx
+++ src/client/routes.jsx
@@ -11,6 +11,7 @@ import SignupSection from './components/auth/SignupSection';
 
 export default (
   <Route path="/" component={Main}>
+    <Route path="/pag/:page" component={Main} />
     <Route path="/signup" component={SignupSection} />
     <Route path="/login" component={LoginSection} />
     <Route path="/frase/:slug" component={QuoteSection} />

b9063d1b4792ddf90b35184ecf1f331b46f512f4
zrobit <luismigre47@gmail.com>
2017-05-24 17:57:01 -0500

    Add pagination

--- src/server/controllers/homeController.js
+++ src/server/controllers/homeController.js
@@ -8,11 +8,22 @@ const ssr = global.ssr
 
 var router = express.Router();
 
+//total quotes 10620
+function HomeController(req, res){
+  let page = req.params.page;
 
-router.get('/', function(req, res){
-  let context = {};
+  if (/^[1-9][0-9]*$/.test(page)){
+    page = parseInt(page)
+  } else {
+    page = 1
+  }
 
-  api.get('/quotes')
+  let context = {
+    prevPage: page === 1 ? 1062 : page - 1,
+    nextPage:  page === 1062 ? 1 : page + 1
+  };
+
+  api.get('/quotes?page=' + page)
     .then(function(response){
       let state = {
         quote: response.data
@@ -20,7 +31,12 @@ router.get('/', function(req, res){
       context.state = state;
       ssr(req, res, context)
     })
-});
+}
 
+
+
+router.get('/', HomeController);
+
+router.get('/pag/:page', HomeController);
 
 module.exports = router;

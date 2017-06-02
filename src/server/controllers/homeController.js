b95a96a00843ade6f736febc5e7830ad498029f4
zrobit <luismigre47@gmail.com>
2017-06-02 17:39:50 -0500

    Add: initial promise handler for metadata

--- src/server/controllers/homeController.js
+++ src/server/controllers/homeController.js
@@ -4,9 +4,33 @@ const api = axios.create({
   baseURL: 'http://localhost:3000/api/'
 });
 
+
+const Meta = require('../models/meta');
+
 const ssr = global.ssr
 
 var router = express.Router();
+
+function getMeta(param){
+  return  new Promise(function(resolve, reject){
+    Meta.findOne({}).exec(function(err, meta){
+      if(err) throw err;
+      resolve(meta);
+    });
+  })
+
+}
+
+function getQuotes(page){
+  return new Promise(function(resolve, reject){
+   api.get('/quotes?page=' + page)
+    .then(function(response){
+      resolve(response.data);
+    })
+
+  })
+}
+
 
 //total quotes 10620
 function HomeController(req, res){
@@ -32,10 +56,34 @@ function HomeController(req, res){
       ssr(req, res, context)
     })
 }
+//TEsting
+//total quotes 10620
+function HomeControllerTest(req, res){
+  let page = req.params.page;
+
+  if (/^[1-9][0-9]*$/.test(page)){
+    page = parseInt(page)
+  } else {
+    page = 1
+  }
+
+  let context = {
+    prevPage: page === 1 ? 1062 : page - 1,
+    nextPage:  page === 1062 ? 1 : page + 1
+  };
+
+  Promise.all([getMeta('hi'), getQuotes(page)]).then(function(values){
+      let state = {
+        quote: values[1]
+      }
+      context.meta = values[0];
+      context.state = state;
+      ssr(req, res, context)
+  })
+}
 
 
-
-router.get('/', HomeController);
+router.get('/', HomeControllerTest);
 
 router.get('/pag/:page', HomeController);
 

2997e64d2097ddfe3747571072546dbf50f366a3
zrobit <luismigre47@gmail.com>
2017-06-03 13:21:49 -0500

    Swith to prev implementation

--- src/server/controllers/quoteController.js
+++ src/server/controllers/quoteController.js
@@ -11,70 +11,71 @@ const ssr = global.ssr
 const router = express.Router();
 
 
-// router.get('/:slug', function(req, res){
-//   let slug = req.params.slug
-
-//   let context = {};
-
-//   api.get('/quotes/' + slug)
-//     .then(function(response){
-//       let state = {
-//         quote: {
-//           detail: response.data.quote
-//         },
-//         author: {
-//           detail: response.data.quote.author
-//         }
-//       }
-//       context.state = state;
-//       ssr(req, res, context)
-//     })
-// });
-
-function getMeta(param){
-  return new Promise((resolve, reject)=>{
-    Meta.findOne({}).exec((err, meta)=>{
-      if (err) throw err;
-      resolve(meta);
-    })
-  })
-}
-
-function getQuote(slug){
-  return new Promise((resolve, reject)=>{
-  api.get(`/quotes/${slug}`)
-    .then(function(response){
-      resolve(response.data)
-    })
-  })
-}
-
-
-function quoteDetailController(req, res){
+router.get('/:slug', function(req, res){
   let slug = req.params.slug
+
   let context = {};
-  Promise.all([getMeta('hi'), getQuote(slug)]).then( values => {
-    let meta = values[0];
-    let quote = values[1];
-    if (quote.quote === null){
-      res.status(404).send('No encontrado')
-    }
 
-    context.meta = values[0];
-    context.state = {
-      quote: {
-        detail: values[1].quote
-      },
-      author: {
-        detail: values[1].quote.author
+  api.get('/quotes/' + slug)
+    .then(function(response){
+      let state = {
+        quote: {
+          detail: response.data.quote
+        },
+        author: {
+          detail: response.data.quote.author
+        }
       }
-    }
-    ssr(req, res, context)
-  })
-}
+      context.state = state;
+      ssr(req, res, context)
+    })
+});
+
+// function getMeta(param){
+//   return new Promise((resolve, reject)=>{
+//     Meta.findOne({}).exec((err, meta)=>{
+//       if (err) throw err;
+//       resolve(meta);
+//     })
+//   })
+// }
+
+// function getQuote(slug){
+//   return new Promise((resolve, reject)=>{
+//   api.get(`/quotes/${slug}`)
+//     .then(function(response){
+//       resolve(response.data)
+//     })
+//   })
+// }
+
+
+// function quoteDetailController(req, res){
+//   let slug = req.params.slug
+//   let context = {};
+//   Promise.all([getMeta('hi'), getQuote(slug)]).then( values => {
+//     const [meta, quote] = values;
+
+//     if (quote.quote === null){
+//       res.status(404).send('No encontrado')
+//     }
+
+//     context.meta = values[0];
+//     context.state = {
+//       quote: {
+//         detail: quote.quote
+//       },
+//       author: {
+//         detail: quote.quote.author
+//       }
+//     }
+//     console.log(JSON.stringify(context.state))
+//     ssr(req, res, context)
+//   })
+// }
 
 
 
-router.get('/:slug', quoteDetailController)
+// router.get('/:slug', quoteDetailController)
 
 module.exports = router;

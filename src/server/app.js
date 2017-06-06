1171769aa4eb2f786c579baa53a05a8c62714102
zrobit <luismigre47@gmail.com>
2017-06-06 10:03:04 -0500

    Refactor to a pure app setup

--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
+
 const passport = require('passport');
 const flash    = require('connect-flash');
 
@@ -8,20 +7,13 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session      = require('express-session');
-mongoose.Promise = global.Promise;
-
-if (process.env.NODE_ENV==='production'){
-  mongoose.connect('mongodb://localhost/fraseary-production');
-
-} else {
-  mongoose.connect('mongodb://localhost/fraseary-local');
-}
 
 var router = require('./router')
 
-app.locals = {pretty:true, cache:true, env: process.env}
-
 require('./config/passport');
+
+const app = express();
+
 
 app.use(morgan('dev'));
 app.use(cookieParser());
@@ -42,23 +34,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-
+app.locals = {pretty:true, cache:true, env: process.env}
 
 app.use(router)
 
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
 
-app.listen(3000, function () {
-  console.log('Server is Ready');
-});
 
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Database is Ready')
-  // we're connected!
-});
-
-
+module.exports = app;

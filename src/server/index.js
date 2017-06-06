247cb6ebb75881736da19c99932c3a48c44ed084
zrobit <luismigre47@gmail.com>
2017-06-06 10:03:55 -0500

    Refactor database parts

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
-var hook = require('css-modules-require-hook');
+//Hook for load css names
+const hook = require('css-modules-require-hook');
 const stylus = require('stylus')
 // const nib = require('nib')
 hook({
@@ -10,6 +11,28 @@ hook({
       .render();
   }
 });
+//end
 
+//Load app
+const app = require('./app.js');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
-require('./app.js')
+//Mongoose configuration
+if (process.env.NODE_ENV==='production'){
+  mongoose.connect('mongodb://localhost/fraseary-production');
+
+} else {
+  mongoose.connect('mongodb://localhost/fraseary-local');
+}
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Database is Ready')
+  // we're connected!
+});
+
+app.listen(3000, function () {
+  console.log('Server is Ready');
+});

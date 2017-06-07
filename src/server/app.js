388dc37977625eef268dbd6ad2ace02ec91a8cb3
zrobit <luismigre47@gmail.com>
2017-06-07 12:11:43 -0500

    Rollback manual and ad support for testing

--- src/server/app.js
+++ src/server/app.js
@@ -1,8 +1,6 @@
-//load hook for styles
-require('./hook')
-
 const express = require('express');
-
+const app = express();
+const mongoose = require('mongoose');
 const passport = require('passport');
 const flash    = require('connect-flash');
 
@@ -10,13 +8,20 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session      = require('express-session');
+mongoose.Promise = global.Promise;
+
+if (process.env.NODE_ENV==='production'){
+  mongoose.connect('mongodb://localhost/fraseary-production');
+
+} else {
+  mongoose.connect('mongodb://localhost/fraseary-local');
+}
 
 var router = require('./router')
 
+app.locals = {pretty:true, cache:true, env: process.env}
+
 require('./config/passport');
-
-const app = express();
-
 
 app.use(morgan('dev'));
 app.use(cookieParser());
@@ -37,12 +42,21 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-app.locals = {pretty:true, cache:true, env: process.env}
+
 
 app.use(router)
 
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
 
+//Database
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Database is Ready')
+  // we're connected!
+});
 
-module.exports = app;
+module.exports.listen = app.listen(3000, function () {
+  console.log('Server is Ready');
+});
--- src/server/index.js
+++ src/server/index.js
@@ -1,25 +1,16 @@
-//Load app
-const app = require('./app.js');
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-
-//Mongoose configuration
-if (process.env.NODE_ENV==='production'){
-  mongoose.connect('mongodb://localhost/fraseary-production');
-
-} else {
-  mongoose.connect('mongodb://localhost/fraseary-local');
-}
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Database is Ready')
-  // we're connected!
+var hook = require('css-modules-require-hook');
+const stylus = require('stylus')
+// const nib = require('nib')
+hook({
+  generateScopedName: '[local]-[hash:base64:5]',
+  extensions: ['.styl'],
+  preprocessCss: function (css, filename) {
+    return stylus(css)
+      .set('filename', filename)
+      .render();
+  }
 });
 
-app.listen(3000, function () {
-  console.log('Server is Ready');
-});
+const app = require('./app');
 
-module.exports = app;
+module.exports = app.listen;

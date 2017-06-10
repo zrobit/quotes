d42bd0851c9e3c5ba25cd3d756e5b2e88ccc1c12
zrobit <luismigre47@gmail.com>
2017-06-09 21:31:56 -0500

    Refactor xo style

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-  , router = express.Router()
+const express = require('express');
 
-router.use('/', require('./homeController'))
-router.use('/frase', require('./quoteController'))
-router.use('/autor', require('./authorController'))
-router.use('/perfil', require('./profileController'))
-router.use('/buscar', require('./searchController'))
-router.use('/', require('./authController'))
+const router = new express.Router();
 
-module.exports = router
+router.use('/', require('./homeController'));
+router.use('/frase', require('./quoteController'));
+router.use('/autor', require('./authorController'));
+router.use('/perfil', require('./profileController'));
+router.use('/buscar', require('./searchController'));
+router.use('/', require('./authController'));
+
+module.exports = router;
--- src/server/controllers/metaController.js
+++ src/server/controllers/metaController.js
@@ -1,12 +1,14 @@
 const Meta = require('../models/meta');
 
-function getMeta(param){
-  return new Promise((resolve, reject)=>{
-    Meta.findOne({}).exec((err, meta)=>{
-      if (err) throw err;
+function getMeta() {
+  return new Promise((resolve, reject) => {
+    Meta.findOne({}).exec((err, meta) => {
+      if (err) {
+        throw err;
+      }
       resolve(meta);
-    })
-  })
+    });
+  });
 }
 
 module.exports = getMeta;

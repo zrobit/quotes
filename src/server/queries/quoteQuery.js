ecf79f899034fc00a27d6fd4b5db15b3f747481a
zrobit <luismigre47@gmail.com>
2017-06-10 12:43:28 -0500

    Refactor code style xo

--- src/server/queries/quoteQuery.js
+++ src/server/queries/quoteQuery.js
@@ -1,50 +1,58 @@
 const Quote = require('../models/quote');
 
-const { getTagBySlug } = require('./tagQuery')
+const {getTagBySlug} = require('./tag-query');
 
-
-function countQuotes(arg={}){
+function countQuotes(arg = {}) {
   return Quote.count(arg).exec();
 }
-function getQuotes(arg={}, nItems=10, nPage=1){
+
+function getQuoteBy(arg = {}) {
+  return Quote
+    .findOne(arg)
+    .select('slug content author size')
+    .populate('author', 'name slug')
+    .exec();
+}
+
+function getQuotes(arg = {}, nItems = 10, nPage = 1) {
   return Quote
     .find(arg)
     .limit(nItems)
-    .sort({'createdAt':-1})
-    .skip((nPage-1)*nItems)
+    .sort({createdAt: -1})
+    .skip((nPage - 1) * nItems)
     .select('slug content author tags size')
     .populate('tags', 'name slug -_id')
     .populate('author', 'name slug')
     .exec();
 }
 
-function getPagQuotes(arg={}, nItems=10, nPage=1){
+function getPagQuotes(arg = {}, nItems = 10, nPage = 1) {
   return Promise.all([
     getQuotes(arg, nItems, nPage),
-    countQuotes(arg),
+    countQuotes(arg)
   ]).then(([quotes, count]) => {
-    let next = count > nItems*nPage ? nPage + 1: null;
+    const next = count > nItems * nPage ? nPage + 1 : null;
     return {
-      count: count,
-      next: next,
-      quotes: quotes
-    }
-  })
+      count,
+      next,
+      quotes
+    };
+  });
 }
 
-function getPagQuotesByTag(slug, nItems=10, nPage=1){
-  return new Promise((resolve, reject) => {
-    getTagBySlug(slug).then((tag) => {
-      getPagQuotes({'tags': tag._id}).then((quotes)=>{
-        resolve ({
-          tag: tag,
+function getPagQuotesByTag(slug, nItems = 10, nPage = 1) {
+  return new Promise(resolve => {
+    getTagBySlug(slug).then(tag => {
+      getPagQuotes({tags: tag._id}, nItems, nPage).then(quotes => {
+        resolve({
+          tag,
           quotes: quotes.quotes,
           next: quotes.next,
           count: quotes.count
-        })
-      })
-    })
-  })
+        });
+      });
+    });
+  });
 }
 
 module.exports.countQuotes = countQuotes;

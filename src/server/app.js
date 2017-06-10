1bb17cb3bf047d66a01231aaee81b354bf9d1b31
zrobit <luismigre47@gmail.com>
2017-06-09 20:19:40 -0500

    Refactor xo style guide

--- src/server/app.js
+++ src/server/app.js
@@ -1,25 +1,26 @@
 const express = require('express');
+
 const app = express();
 const mongoose = require('mongoose');
 const passport = require('passport');
-const flash    = require('connect-flash');
+const flash = require('connect-flash');
 
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const session      = require('express-session');
+const session = require('express-session');
+
 mongoose.Promise = global.Promise;
 
-if (process.env.NODE_ENV==='production'){
+if (process.env.NODE_ENV === 'production') {
   mongoose.connect('mongodb://localhost/fraseary-production');
-
 } else {
   mongoose.connect('mongodb://localhost/fraseary-local');
 }
 
-var router = require('./router')
+const router = require('./router');
 
-app.locals = {pretty:true, cache:true, env: process.env}
+app.locals = {pretty: true, cache: true, env: process.env};
 
 require('./config/passport');
 
@@ -28,35 +29,32 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(bodyParser.json()); // support json encoded bodies
-
+app.use(bodyParser.json()); // Support json encoded bodies
 
 // required for passport
 app.use(session({
   secret: 'thesewordsareuniquedontcopyXD',
   resave: true,
   saveUninitialized: true
-})); // session secret
+})); // Session secret
 
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
+app.use(router);
 
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 
-app.use(router)
-
-app.set('view engine', 'pug')
-app.set('views', __dirname + '/views')
-
-//Database
-var db = mongoose.connection;
+// Database
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Database is Ready')
-  // we're connected!
+db.once('open', () => {
+  console.log('Database is Ready');
+  // We're connected!
 });
 
-module.exports.listen = app.listen(3000, function () {
+module.exports.listen = app.listen(3000, () => {
   console.log('Server is Ready');
 });

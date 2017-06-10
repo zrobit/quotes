fafaf5abfad24dee05db710062832c8d275b368b
zrobit <luismigre47@gmail.com>
2017-06-09 20:33:24 -0500

    Refactor xo style

--- src/server/router.js
+++ src/server/router.js
@@ -1,18 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 
-if (process.env.NODE_ENV==='production'){
-  global.ssr = require('../../dist/shared/ssr').ssr
+const router = new express.Router();
 
+if (process.env.NODE_ENV === 'production') {
+  global.ssr = require('../../dist/shared/ssr').ssr;
 } else {
-  global.ssr = require('../shared/ssr').ssr
-  router.use('/assets', require('./middleware/static'))
+  global.ssr = require('../shared/ssr').ssr;
+  router.use('/assets', require('./middleware/static'));
 }
 
+router.use('/api', require('./api'));
 
-router.use('/api', require('./api'))
+router.use('/', require('./controllers'));
 
-router.use('/', require('./controllers'))
-
-
-module.exports = router
+module.exports = router;

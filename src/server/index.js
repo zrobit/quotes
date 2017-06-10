82bd32b3170a02f3832d52c1c019f533952d81bc
zrobit <luismigre47@gmail.com>
2017-06-09 20:34:14 -0500

    Refactor xo style

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,10 @@
-var hook = require('css-modules-require-hook');
-const stylus = require('stylus')
-// const nib = require('nib')
+const hook = require('css-modules-require-hook');
+const stylus = require('stylus');
+// #const nib = require('nib')
 hook({
   generateScopedName: '[local]-[hash:base64:5]',
   extensions: ['.styl'],
-  preprocessCss: function (css, filename) {
+  preprocessCss(css, filename) {
     return stylus(css)
       .set('filename', filename)
       .render();

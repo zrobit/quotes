b9ebd2a99ae4d85dadfcfd516fcb477dc6dad8be
zrobit <luismigre47@gmail.com>
2017-06-10 23:21:51 -0500

    Refactor to quote-query.js

--- src/server/api/quote-api.js
+++ src/server/api/quote-api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const words = require('lodash').words;
 const slug = require('slugg');
+
+const {getPagQuotes} = require('../queries/quote-query');
 const Quote = require('../models/quote');
 const Author = require('../models/author');
 const isPage = require('../middleware/isPage');
@@ -11,23 +13,8 @@ const router = new express.Router();
 router.get('/', isPage, (req, res) => {
   const nPage = parseInt(req.query.page, 10);
   const nItems = 10;
-  Quote.count({}, (err, count) => {
-    if (err) {
-      throw err;
-    }
-    const next = count > nItems * nPage ? nPage + 1 : null;
-    const query = Quote.find({}).limit(10).sort({createdAt: -1});
-    query.skip((nPage - 1) * nItems);
-    query.select('slug content author tags size');
-    query.populate('tags', 'name slug -_id');
-    query.populate('author', 'name slug');
-    query.exec((err, data) => {
-      if (err) {
-        throw err;
-      }
-      const context = {quotes: data, next};
-      res.json(context);
-    });
+  getPagQuotes({}, nItems, nPage).then(data => {
+    res.json(data);
   });
 });
 
@@ -35,24 +22,8 @@ router.get('/', isPage, (req, res) => {
 router.get('/author/:id', isPage, (req, res) => {
   const nPage = parseInt(req.query.page, 10);
   const nItems = 10;
-  Quote.count({author: req.params.id}, (err, count) => {
-    if (err) {
-      throw err;
-    }
-    const next = count > nItems * nPage ? nPage + 1 : null;
-    const query = Quote.find({author: req.params.id}).limit(10).sort({createdAt: -1});
-
-    query.skip((nPage - 1) * nItems);
-    query.select('slug content author tags size');
-    query.populate('tags', 'name slug -_id');
-    query.populate('author', 'name slug hashId');
-    query.exec((err, data) => {
-      if (err) {
-        throw err;
-      }
-      const context = {quotes: data, next};
-      res.json(context);
-    });
+  getPagQuotes({author: req.params.id}, nItems, nPage).then(data => {
+    res.json(data);
   });
 });
 

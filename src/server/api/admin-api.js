2a6889664ddce82d7f3bb8a7fd5c8e07bf0f896b
zrobit <luismigre47@gmail.com>
2017-06-19 18:59:35 -0500

    Add admin-api.js for api entry point

---
+++ src/server/api/admin-api.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const {
+  getQuotesAdmin,
+  getQuoteByIdAdmin,
+  countQuotes
+} = require('../queries/quote-query');
+
+const {
+  getAuthorsAdmin,
+  getAuthorByIdAdmin,
+  countAuthors
+} = require('../queries/author-query');
+
+const router = new express.Router();
+
+// #api/admin/quotes
+router.get('/quotes', (req, res) => {
+  const start = parseInt(req.query._start, 10) || 0;
+  const end = parseInt(req.query._end, 10) || 9;
+
+  Promise.all([
+    countQuotes(),
+    getQuotesAdmin({}, start, end)
+  ]).then(([count, quotes]) => {
+    res.header('X-Total-Count', count);
+    res.json(quotes);
+  });
+});
+
+router.get('/quotes/:id', (req, res) => {
+  getQuoteByIdAdmin(req.params.id).then(data => {
+    res.json(data);
+  });
+});
+
+// #api/admin/authors
+router.get('/authors', (req, res) => {
+  const start = parseInt(req.query._start, 10) || 0;
+  const end = parseInt(req.query._end, 10) || 9;
+
+  Promise.all([
+    countAuthors(),
+    getAuthorsAdmin({}, start, end)
+  ]).then(([count, authors]) => {
+    res.header('X-Total-Count', count);
+    res.json(authors);
+  });
+});
+
+router.get('/authors/:id', (req, res) => {
+  getAuthorByIdAdmin(req.params.id).then(data => {
+    res.json(data);
+  });
+});
+
+module.exports = router;

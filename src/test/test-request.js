def5acca424a1d248dfaf5cd69699e48dc01a4f8
zrobit <luismigre47@gmail.com>
2017-06-30 21:14:27 -0500

    Change: test for ephimeral database | test-request.js

--- src/test/test-request.js
+++ src/test/test-request.js
@@ -1,8 +1,44 @@
 import test from 'ava';
+import mongoose from 'mongoose';
 
 import request from 'supertest';
-
 import app from '../server';
+import {Author, Quote, Tag} from '../server/models';
+import {cicle} from '../utils';
+import {fakeAuthors, fakeQuotes, fakeTags} from '../utils/data';
+
+test.before('Loading Fake Data', async t => {
+  // This runs after the above, but before tests
+  const authors = fakeAuthors(10);
+  const it = cicle(authors);
+  const quotes = fakeQuotes(20);
+  const tags = fakeTags(10);
+
+  await Tag.create(tags, function(err, tags){
+    if(err) throw err;
+    let itTags = cicle(tags);
+    quotes.forEach((element) => {
+      Author.create(it.next(), (err, author)=> {
+        if (err) throw err;
+        element.author = author.id
+        Quote.create(element, (err, quote) => {
+          if (err) throw err;
+          for(let i = 0; i < 3; i++){
+            quote.tags.push(itTags.next());
+          }
+          quote.save(function(err, finalData){
+            console.log(`Quote.slug: ${finalData.slug}`);
+          });
+        });
+      });
+    });
+  });
+});
+
+test.after('cleanup data base', async t => {
+  await mongoose.connection.db.dropDatabase();
+  t.pass();
+});
 
 test('get:home', async t => {
   const res = await request(app).get('/');
@@ -16,7 +52,9 @@ test('get:home', async t => {
 });
 
 test('get:quote:detail', async t => {
-  const res = await request(app).get('/frase/util-es-todo-lo-que-nos-da');
+  const quote = await Quote.findOne().exec();
+  console.log(`Quote.slug: ${quote.slug}`);
+  const res = await request(app).get(`/frase/${quote.slug}`);
 
   t.is(res.status, 200);
   t.is(res.statusCode, 200);
@@ -37,9 +75,10 @@ test('get:quote:undefined:detail', async t => {
   t.true(res.clientError);
 });
 
-
 test('get:author:detail', async t => {
-  const res = await request(app).get('/autor/auguste-rodin');
+  const author = await Author.findOne().exec();
+  console.log(`Author.slug: ${author.slug}`);
+  const res = await request(app).get(`/autor/${author.slug}`);
 
   t.is(res.status, 200);
   t.is(res.statusCode, 200);

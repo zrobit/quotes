017264b3dbbbdf01ee9a81f7b83634119113da23
zrobit <luismigre47@gmail.com>
2017-07-13 17:29:50 -0500

    Refactor: by xo styling

--- src/shared/ssr.js
+++ src/shared/ssr.js
@@ -1,46 +1,45 @@
 import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { match, RouterContext } from 'react-router';
-import { Provider, useStaticRendering } from 'mobx-react';
+import {renderToString} from 'react-dom/server';
+import {match, RouterContext} from 'react-router';
+import {Provider, useStaticRendering} from 'mobx-react';
 
-import routes from '../client/routes';
 import jsonStringifySafe from 'json-stringify-safe';
+import routes from '../client/routes';
 
-
-import createStores from '../client/stores/'
+import createStores from '../client/stores/';
 
 useStaticRendering(true);
 
-export function ssr(req, res, context, template="layout") {
-  match({ routes, location: req.originalUrl },
+export function ssr(req, res, context, template = 'layout') {
+  match({routes, location: req.originalUrl},
     (err, redirect, props) => {
-      if (err){
-        // handleError(res, err);
-        console.log(err)
-      }
-      else if (props) {
-        if(!context.state.user){context.state.user = {} }
-
-        context.state.user.isAuth = req.isAuthenticated();
-        if(context.state.user.isAuth){
-          context.state.user.name = req.user.name
-          context.state.user.hashId = req.user.hashId
+      if (err) {
+        console.log(err);
+      } else if (props) {
+        if (!context.state.user) {
+          context.state.user = {};
         }
 
-        let stores = createStores(context.state)
+        context.state.user.isAuth = req.isAuthenticated();
+        if (context.state.user.isAuth) {
+          context.state.user.name = req.user.name;
+          context.state.user.hashId = req.user.hashId;
+        }
+
+        const stores = createStores(context.state);
         try {
           context.root = renderToString(
             <Provider {...stores}>
-              <RouterContext {...props} />
+              <RouterContext {...props}/>
             </Provider>
-          )
-        } catch (e) {
-          console.error(e);
+          );
+        } catch (err) {
+          console.error(err);
         }
 
-        context.initialStateJSON = jsonStringifySafe(context.state)
+        context.initialStateJSON = jsonStringifySafe(context.state);
 
-        res.render(template, context)
+        res.render(template, context);
       }
     });
 }

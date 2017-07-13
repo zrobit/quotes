43b968b425be1c974d830ff18698ed4280472458
zrobit <luismigre47@gmail.com>
2017-07-13 16:55:13 -0500

    Add incomplete meta schema

--- src/server/models/meta.js
+++ src/server/models/meta.js
@@ -16,20 +16,18 @@ const metaSchema = new Schema({
   og: {
     title: {type: String, maxlength: 90},
     description: {type: String, maxlength: 200}
-  },
-  slug: {type: String, required: true, unique: true, index: true},
-  quote: {type: Schema.Types.ObjectId, ref: 'Quote'}
+  }
 }, config);
 
-metaSchema.virtual('og').get(() => {
-  return {
-    url: 'http://www.fraseary.com',
-    title: 'Fraseary',
-    type: 'website',
-    description: 'descript... for fraseary web',
-    image: 'http://media.fraseary.com/fraseary-logo-1200x1200.png'
-  };
-});
+// metaSchema.virtual('og').get(() => {
+//   return {
+//     url: 'http://www.fraseary.com',
+//     title: 'Fraseary',
+//     type: 'website',
+//     description: 'descript... for fraseary web',
+//     image: 'http://media.fraseary.com/fraseary-logo-1200x1200.png'
+//   };
+// });
 
 const Meta = mongoose.model('Meta', metaSchema);
 module.exports = Meta;

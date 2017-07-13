4ca8d5d8f25090739f7accafd4755da54859799f
zrobit <luismigre47@gmail.com>
2017-07-13 16:55:39 -0500

    Add reusable schema for metas

---
+++ src/server/models/meta-schema.js
@@ -0,0 +1,8 @@
+module.exposts = {
+  title: {type: String, maxlength: 90},
+  description: {type: String, maxlength: 200},
+  og: {
+    title: {type: String, maxlength: 90},
+    description: {type: String, maxlength: 200}
+  }
+};

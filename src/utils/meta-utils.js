77584abf3de4700da3c9fb2639b18c9eb470b70c
zrobit <luismigre47@gmail.com>
2017-07-14 12:47:55 -0500

    Add meta handler

---
+++ src/utils/meta-utils.js
@@ -0,0 +1,25 @@
+function metaUtils(meta = {}) {
+  const _meta = {
+    title: 'Fraseary Palabras Frases Citas Refranes y Proverbios',
+    description: 'Fraseary es un repositorio de frases citas refranes y proverbios que inspiran a pensar sobre amor amistad humanidad',
+    url: 'http://fraseary.com'
+  };
+  if (!meta.og) {
+    meta.og = {
+      title: _meta.title,
+      description: _meta.description
+    };
+  }
+
+  return {
+    title: meta.title || _meta.title,
+    description: meta.description || _meta.description,
+    url: meta.url ? _meta.url + meta.url : _meta.url,
+    og: {
+      title: meta.og.title || _meta.title,
+      description: meta.og.description || _meta.description
+    }
+  };
+}
+
+module.exports = metaUtils;

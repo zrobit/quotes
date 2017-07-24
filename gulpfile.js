c9aa8afaf77db8d54fa894ad1df5e17b21ced97c
zrobit <luismigre47@gmail.com>
2017-07-23 19:04:28 -0500

    update: for gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ var gulp = require('gulp');
 if (process.env.NODE_ENV === 'development') {
   require("./tasks/seed.js");
   require("./tasks/populate.js");
+  require("./tasks/sitemap.js");
 }
 // require("./tasks/watch.js");
 // require("./tasks/scripts.js");
 // require("./tasks/icons.js");
 // require("./tasks/iconfonts.js");
-// require("./tasks/styles.js");
-// require("./tasks/copy.js");
+require("./tasks/styles.js");
+require("./tasks/copy.js");
 
 
-require("./tasks/sitemap.js");
 
 
 

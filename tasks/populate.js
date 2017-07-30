e4abb73b6f2530cffec4307c946fbc982e8954f0
zrobit <luismigre47@gmail.com>
2017-07-30 17:05:08 -0500

    Lint code populate.js sitemap.js

--- tasks/populate.js
+++ tasks/populate.js
@@ -1,187 +1,203 @@
+const fs = require('fs');
+const readline = require('readline');
+const Stream = require('stream');
+const slug = require('slugg');
 const gulp = require('gulp');
+const mongoose = require('mongoose');
 
-var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/fraseary-local-copy');
-// mongoose.set('debug', true);
+// #mongoose.set('debug', true);
 
 const Author = require('../src/server/models/author');
 const Quote = require('../src/server/models/quote');
 const Meta = require('../src/server/models/meta');
 const Tag = require('../src/server/models/tag');
 
-var fs = require('fs');
-var readline = require('readline');
-var stream = require('stream');
-
-var slug = require('slugg');
-
-
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-gulp.task('connectDB', function(cb) {
-  db.once('open', function() {
-    console.log('========== Database is Ready =======')
+gulp.task('connectDB', cb => {
+  db.once('open', () => {
+    console.log('========== Database is Ready =======');
     cb();
   });
 });
 
-gulp.task('populate:quotes', ['connectDB'], function(){
-  console.log('Updating quotes...')
-  var instream = fs.createReadStream('../data/quotes.alfa.txt');
-  var outstream = new stream;
-  var rl = readline.createInterface(instream, outstream);
-  rl.on('line', function(line) {
-    // process line here
+gulp.task('populate:quotes', ['connectDB'], () => {
+  console.log('Updating quotes...');
+  const instream = fs.createReadStream('../data/quotes.alfa.txt');
+  const outstream = new Stream();
+  const rl = readline.createInterface(instream, outstream);
+  rl.on('line', line => {
+    // Process line here
     line = line.trim();
 
-    if(line === ''){
+    if (line === '') {
       return;
     }
-    let quoteSlug = slug(line)
+    const quoteSlug = slug(line);
 
     Meta.findOne({slug: quoteSlug}, (err, data) => {
-        if (err) throw err;
-        if(!data){
-          Quote.create({content: line}, (err, quote) => {
-            if (err) throw err;
-            Meta.create({slug: quoteSlug, quote: quote.id}, (err, meta)=>{
-              if (err) throw err;
-              console.log(meta);
-            })
-          })
-        }
-      });
+      if (err) {
+        throw err;
+      }
+      if (!data) {
+        Quote.create({content: line}, (err, quote) => {
+          if (err) {
+            throw err;
+          }
+          Meta.create({slug: quoteSlug, quote: quote.id}, (err, meta) => {
+            if (err) {
+              throw err;
+            }
+            console.log(meta);
+          });
+        });
+      }
+    });
   });
 
-  rl.on('close', function() {
-    // do something on finish here
-    console.log('Close file')
+  rl.on('close', () => {
+    // Do something on finish here
+    console.log('Close file');
   });
 });
 
-gulp.task('populate:authors', ['connectDB'], function(){
-  console.log('Updating authors...')
-  var instream = fs.createReadStream('../data/authors.alfa.txt');
-  var outstream = new stream;
-  var rl = readline.createInterface(instream, outstream);
-  rl.on('line', function(line) {
-    // process line here
+gulp.task('populate:authors', ['connectDB'], () => {
+  console.log('Updating authors...');
+  const instream = fs.createReadStream('../data/authors.alfa.txt');
+  const outstream = new Stream();
+  const rl = readline.createInterface(instream, outstream);
+  rl.on('line', line => {
+    // Process line here
     line = line.trim();
 
-    if(line === ''){
+    if (line === '') {
       return;
     }
-    Author.getOrCreate({name: line}, function(err, data){
-      if (err) throw err;
-      let object = JSON.stringify(data, null, 2)
+    Author.getOrCreate({name: line}, (err, data) => {
+      if (err) {
+        throw err;
+      }
+      const object = JSON.stringify(data, null, 2);
       console.log(object);
     });
   });
 
-  rl.on('close', function() {
-    // do something on finish here
-    console.log('Close file')
+  rl.on('close', () => {
+    // Do something on finish here
+    console.log('Close file');
   });
 });
 
-gulp.task('populate:tags', ['connectDB'], function(){
-  console.log('Updating tags...')
-  var instream = fs.createReadStream('../data/tags.alfa.txt');
-  var outstream = new stream;
-  var rl = readline.createInterface(instream, outstream);
-  rl.on('line', function(line) {
-    // process line here
+gulp.task('populate:tags', ['connectDB'], () => {
+  console.log('Updating tags...');
+  const instream = fs.createReadStream('../data/tags.alfa.txt');
+  const outstream = new Stream();
+  const rl = readline.createInterface(instream, outstream);
+  rl.on('line', line => {
+    // Process line here
     line = line.trim();
 
-    if(line === ''){
+    if (line === '') {
       return;
     }
 
-    Tag.getOrCreate({name: line}, function(err, data){
-      if (err) throw err;
-      let object = JSON.stringify(data, null, 2)
+    Tag.getOrCreate({name: line}, (err, data) => {
+      if (err) {
+        throw err;
+      }
+      const object = JSON.stringify(data, null, 2);
       console.log(object);
     });
   });
 
-  rl.on('close', function() {
-    // do something on finish here
-    console.log('Close file')
+  rl.on('close', () => {
+    // Do something on finish here
+    console.log('Close file');
   });
 });
 
-
-gulp.task('populate:joins', ['connectDB'], function(){
-  console.log('Updating joins...')
-  var instream = fs.createReadStream('../data/__quotes__.json');
-  var outstream = new stream;
-  var rl = readline.createInterface(instream, outstream);
-  rl.on('line', function(line) {
-    // process line here
+gulp.task('populate:joins', ['connectDB'], () => {
+  console.log('Updating joins...');
+  const instream = fs.createReadStream('../data/__quotes__.json');
+  const outstream = new Stream();
+  const rl = readline.createInterface(instream, outstream);
+  rl.on('line', line => {
+    // Process line here
     line = line.trim();
-    if(line === ''|| line=== '[' || line===']'){
+    if (line === '' || line === '[' || line === ']') {
       return;
     }
-    if(line.slice(-1)===','){
-      line = line.slice(0, -1)
+    if (line.slice(-1) === ',') {
+      line = line.slice(0, -1);
     }
-    line = JSON.parse(line)
-    let quoteSlug = slug(line.content);
-    let authorSlug = slug(line.author);
-    let tagSlug = slug(line.category);
+    line = JSON.parse(line);
+    const quoteSlug = slug(line.content);
+    const authorSlug = slug(line.author);
+    const tagSlug = slug(line.category);
 
-
-    Meta.findOne({slug: quoteSlug}, (err, meta)=>{
-      if(err) throw err;
-      if(!meta) return;
-      Quote.findOne({_id: meta.quote}, (err, quote)=>{
-        if(!quote.author){
-          Author.findOne({slug: authorSlug}, (err, author)=>{
-            if(err) throw err;
-            if(author===null){
-              console.log(line.author)
+    Meta.findOne({slug: quoteSlug}, (err, meta) => {
+      if (err) {
+        throw err;
+      }
+      if (!meta) {
+        return;
+      }
+      Quote.findOne({_id: meta.quote}, (err, quote) => {
+        if (!quote.author) {
+          Author.findOne({slug: authorSlug}, (err, author) => {
+            if (err) {
+              throw err;
+            }
+            if (author === null) {
+              console.log(line.author);
             }
             quote.author = author.id;
             quote.save();
-            console.log('quotes+ author:'+ quote)
-          })
+            console.log('quotes+ author:' + quote);
+          });
         }
 
-        Tag.findOne({slug: tagSlug}, (err, tag)=>{
-
-          if (err) throw err;
-          quote.tags.addToSet(tag.id)
-          quote.save()
-          console.log('quotes+ tags:'+ quote)
-        })
-      })
-    })
+        Tag.findOne({slug: tagSlug}, (err, tag) => {
+          if (err) {
+            throw err;
+          }
+          quote.tags.addToSet(tag.id);
+          quote.save();
+          console.log('quotes+ tags:' + quote);
+        });
+      });
+    });
   });
 
-  rl.on('close', function() {
-    // do something on finish here
-    console.log('Close file')
+  rl.on('close', () => {
+    // Do something on finish here
+    console.log('Close file');
   });
 });
 
-
-gulp.task('populate:tags:recurrence', ['connectDB'], function(){
-  console.log('Updating tags...')
-  Tag.find({}, function(err, tags){
-    if (err) throw err;
-    tags.map(function(tag){
-      Quote.count({'tags': tag.id}, function(err, count){
-        if (err) throw err;
-        Tag.findOneAndUpdate({_id:tag.id}, {recurrence:count},function(err, data){
-          if (err) throw err;
-          console.log(data.name + ' num: '+count)
-        })
-
-      })
-    })
-  })
+gulp.task('populate:tags:recurrence', ['connectDB'], () => {
+  console.log('Updating tags...');
+  Tag.find({}, (err, tags) => {
+    if (err) {
+      throw err;
+    }
+    tags.forEach(tag => {
+      Quote.count({tags: tag.id}, (err, count) => {
+        if (err) {
+          throw err;
+        }
+        Tag.findOneAndUpdate({_id: tag.id}, {recurrence: count}, (err, data) => {
+          if (err) {
+            throw err;
+          }
+          console.log(data.name + ' num: ' + count);
+        });
+      });
+    });
+  });
 });
 
 gulp.task('populate:meta:quote', ['connectDB'], async () => {
--- tasks/sitemap.js
+++ tasks/sitemap.js
@@ -1,28 +1,26 @@
+const fs = require('fs');
 const gulp = require('gulp');
 const sm = require('sitemap');
-const fs = require('fs');
 
-
-function siteMapHome(){
+function siteMapHome() {
   console.log('Generating sitemap-home.xml');
 
-  let urls = [...Array(1000)].map((item, i) => {
+  const urls = [...Array(1000)].map((item, i) => {
     return {
-      url: '/pag/'+(i+1),
+      url: '/pag/' + (i + 1),
       changefreq: 'monthly',
       lastmodISO: '2017-04-10T22:32:50.764Z',
       priority: 0.5
     };
-  })
+  });
 
-  let options = {
+  const options = {
     hostname: 'http://www.fraseary.com',
-    urls: urls
+    urls
   };
 
-  var sitemap = sm.createSitemap(options)
-  fs.writeFileSync("../data/sitemaps/sitemap-home.xml", sitemap.toString());
+  const sitemap = sm.createSitemap(options);
+  fs.writeFileSync('../data/sitemaps/sitemap-home.xml', sitemap.toString());
 }
-
 
 gulp.task('sitemap:home', siteMapHome);

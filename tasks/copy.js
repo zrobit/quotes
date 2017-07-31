e60961578b09aee0e0cbe8d5a1505b68b10d04c6
zrobit <luismigre47@gmail.com>
2017-07-30 22:13:04 -0500

    Lint code

--- tasks/copy.js
+++ tasks/copy.js
@@ -1,68 +1,40 @@
-// jshint esversion: 6
-var gulp = require('gulp'),
-  // plumber = require('gulp-plumber'),+
-  // changed = require('gulp-changed-in-place'),
-  merge = require('merge-stream');
-  // livereload = require('gulp-livereload');
-
+const gulp = require('gulp');
+const merge = require('merge-stream');
 
 const src = {
   images: ['src/client/assets/images/**/*', '!src/client/assets/images/icons/**/*'],
   fonts: ['src/client/assets/fonts/**/*']
 };
 
-
 const dest = {
   images: 'dist/public/assets/images/',
   fonts: 'dist/public/assets/fonts/'
 };
 
-function styles (){
-  var main = gulp
+function styles() {
+  const main = gulp
     .src(['src/client/assets/styles/**/*.styl'])
     .pipe(gulp.dest('dist/client/assets/styles/'));
 
-  var components = gulp
+  const components = gulp
     .src(['src/client/components/**/*.styl'])
     .pipe(gulp.dest('dist/client/components/'));
 
   return merge(main, components);
 }
-function templates (){
+
+function templates() {
   return gulp
     .src(['src/server/views/**/*.pug'])
-    .pipe(gulp.dest('dist/server/views/'))
+    .pipe(gulp.dest('dist/server/views/'));
 }
-function images (){
+
+function images() {
   return gulp
     .src(src.images)
     .pipe(gulp.dest(dest.images));
 }
 
-
-
-/*
-function scripts (){
-  return gulp
-    .src(src.scripts)
-    // .pipe(plumber())
-    // .pipe(changed())
-    .pipe(gulp.dest(dest.scripts))
-    .pipe(livereload());
-}
-
-
-
-
-
-function fonts (){
-  return gulp
-    .src(src.fonts)
-    .pipe(gulp.dest(dest.fonts));
-}
-*/
-
-// gulp.task('copy:scripts', scripts);
 // gulp.task('copy:images', images);
 // gulp.task('copy:fonts', fonts);
 gulp.task('copy:styles', styles);

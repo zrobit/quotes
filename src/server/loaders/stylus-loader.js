5d308a3f47fa26e9f60f473237b0dc648297d9f8
zrobit <luismigre47@gmail.com>
2017-07-31 17:19:29 -0500

    Delete unnecessary file

--- src/server/loaders/stylus-loader.js
+++
@@ -1,7 +0,0 @@
-const stylus = require('stylus');
-
-module.exports = (data, file) => {
-  return stylus(data).set('filename', file).render();
-  // return stylus(str).set('filename', path).use(nib());
-
-};

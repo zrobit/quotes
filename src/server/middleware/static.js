a72472c856a836ddb9a707b18452bd0f8d538c16
zrobit <luismigre47@gmail.com>
2017-07-31 17:18:00 -0500

    Lint static.js

--- src/server/middleware/static.js
+++ src/server/middleware/static.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var stylus = require('stylus');
-var nib = require('nib');
-var config = require('../../config')
+const express = require('express');
+const stylus = require('stylus');
+const nib = require('nib');
+const config = require('../../config');
 
-var router = express.Router();
+const router = new express.Router();
 
 router.use('/styles', stylus.middleware({
   debug: true,
@@ -11,21 +11,14 @@ router.use('/styles', stylus.middleware({
   dest: config.PUBLIC_STYLES,
   serve: true,
   forse: false,
-  compile: function (str, path) {
+  compile(str, path) {
     return stylus(str).set('filename', path).use(nib());
   }
 }));
 
-// router.use('/styles', function(req, res){
-
-//   res.json(__dirname + '/../../build/public/assets/styles')
-
-// });
 router.use('/styles', express.static(config.PUBLIC_STYLES));
-// router.use('/scripts', express.static(__dirname + '/../../build/public/assets/scripts'));
 router.use('/images', express.static(config.ASSETS_IMAGES));
 router.use('/media/images', express.static(config.MEDIA_IMAGES));
 router.use('/fonts', express.static(config.ASSETS_FONTS));
-
 
 module.exports = router;

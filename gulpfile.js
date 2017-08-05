ff088b8c1e2dcfa6040233a24bd04482a49261bc
zrobit <luismigre47@gmail.com>
2017-08-05 12:28:20 -0500

    Lint code and add exception for lint gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,10 @@
-var gulp = require('gulp');
+/* eslint import/no-unassigned-import: 0 */
+const gulp = require('gulp');
 
-//Develpment tasks
-if (process.env.NODE_ENV === 'development') {
-  require("./tasks/seed.js");
-  require("./tasks/populate.js");
-  require("./tasks/sitemap.js");
-}
-// require("./tasks/watch.js");
-// require("./tasks/scripts.js");
-// require("./tasks/icons.js");
-// require("./tasks/iconfonts.js");
-require("./tasks/styles.js");
-require("./tasks/copy.js");
+require('./tasks/seed.js');
+require('./tasks/populate.js');
+require('./tasks/sitemap.js');
+require('./tasks/styles.js');
+require('./tasks/copy.js');
 
-
-
-
-
-// gulp.task('copy', ['copy:assets']);
-
-// gulp.task('dev', ['templates', 'styles', 'copy']);
-
-// gulp.task('deploy', ['templates', 'styles:deploy', 'scripts']);
-
-// gulp.task('default', ['dev']);
+gulp.task('copy', ['copy:assets']);

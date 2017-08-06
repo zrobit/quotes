8f9aeb95dd756e6570fdec2a0a8264a6505ef236
zrobit <luismigre47@gmail.com>
2017-08-05 22:38:42 -0500

    Update ssr.js to react-router v4

--- src/shared/ssr.js
+++ src/shared/ssr.js
@@ -1,47 +1,43 @@
 import React from 'react';
 import {renderToString} from 'react-dom/server';
-import {match, RouterContext} from 'react-router';
+import {StaticRouter} from 'react-router-dom';
 import {Provider, useStaticRendering} from 'mobx-react';
 import jsonStringifySafe from 'json-stringify-safe';
 
-import routes from '../client/routes';
+import AppRoutes from '../client/routes';
+
 import createStores from '../client/stores/';
 import metaUtils from './meta-utils';
 
 useStaticRendering(true);
 
 function ssr(req, res, context, template = 'layout') {
-  match({routes, location: req.originalUrl},
-    (err, redirect, props) => {
-      if (err) {
-        console.log(err);
-      } else if (props) {
-        if (!context.state.user) {
-          context.state.user = {};
-        }
+  if (!context.state.user) {
+    context.state.user = {};
+  }
 
-        context.state.user.isAuth = req.isAuthenticated();
-        if (context.state.user.isAuth) {
-          context.state.user.name = req.user.name;
-          context.state.user.hashId = req.user.hashId;
-        }
+  context.state.user.isAuth = req.isAuthenticated();
+  if (context.state.user.isAuth) {
+    context.state.user.name = req.user.name;
+    context.state.user.hashId = req.user.hashId;
+  }
 
-        const stores = createStores(context.state);
-        try {
-          context.root = renderToString(
-            <Provider {...stores}>
-              <RouterContext {...props}/>
-            </Provider>
-          );
-        } catch (err) {
-          console.error(err);
-        }
+  const stores = createStores(context.state);
+  try {
+    context.root = renderToString(
+      <Provider {...stores}>
+        <StaticRouter location={req.originalUrl} context={{}}>
+          <AppRoutes/>
+        </StaticRouter>
+      </Provider>
+    );
+  } catch (err) {
+    console.error(err);
+  }
 
-        context.initialStateJSON = jsonStringifySafe(context.state);
-        context.meta = metaUtils(context.meta || undefined);
-        res.render(template, context);
-      }
-    });
+  context.initialStateJSON = jsonStringifySafe(context.state);
+  context.meta = metaUtils(context.meta || undefined);
+  res.render(template, context);
 }
 
 module.exports = ssr;

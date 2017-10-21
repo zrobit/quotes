2f3f15c040ab54274c369c060858649ab5196096
zrobit <luismigre47@gmail.com>
2017-10-20 21:29:25 -0500

    Switch off seed tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 /* eslint import/no-unassigned-import: 0 */
 const gulp = require('gulp');
 
-require('./tasks/seed.js');
-require('./tasks/populate.js');
-require('./tasks/sitemap.js');
+// require('./tasks/seed.js');
+// require('./tasks/populate.js');
+// require('./tasks/sitemap.js');
 require('./tasks/styles.js');
 require('./tasks/copy.js');
 
